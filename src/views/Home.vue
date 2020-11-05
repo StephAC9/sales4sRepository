3f86a9f4c1499d9b174e69002ca5270a06ed2ddc
Steph <37844889+StephAC9@users.noreply.github.com>
2020-11-05 17:27:24 +0100

    Sign up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
+    <SignUp class="sign-up"></SignUp>
   </div>
 </template>
 
 <script>
-
+import SignUp from '../components/SignUp'
 export default {
+  components:{
+    SignUp
+  }
 }
 </script>
+<style scoped>
+  .sign-up{
+    max-width: 500px;
+    border-radius: 10px;
+    background: rgb(233, 229, 229);
+    margin: 0 auto;
+    margin-top: 50px;
+    padding: 15px;
+  }
+</style>
