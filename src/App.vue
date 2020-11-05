9882ba32ad2eb3a5a5c51a71e357013bdbd9acb9
johntell <john_tell92@outlook.com>
2020-11-05 18:56:03 +0100

    Post to DE added

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,45 @@
   <div id="app">
     <div id="nav">
       <router-link to="/"></router-link>
-      <router-link to="/"></router-link>
+      <input v-model="name" type="text">
+      <button @click="postToDE">Send data to DE</button>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import axios from 'axios' 
+  export default {
+    data() {
+      return {
+        name: null,
+        token: null
+      }
+    },
+    created () {
+      this.initAuth();
+    },
+    methods: {
+      initAuth() {
+        const authUrl = "http://localhost:3000/auth"
+        axios.get(authUrl)
+        .then((response) => {
+          if(response.data.token)
+            this.token = response.data.token.access_token
+          else
+            console.log("something went wrong...")
+        })
+      },
+      postToDE() {
+        const postUrl = "http://localhost:3000/post"
+        axios.post(postUrl, {token: this.token, form: {name: this.name}}, {contentType: "application/json"})
+          .then(console.log)
+      }
+    },
+  }
+</script>
+
 
 <style lang="scss">
 </style>
--- src/server.js
+++ src/server.js
@@ -3,26 +3,21 @@ const axios = require('axios')
 const app = express()
 require('dotenv').config()
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-//   res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header,Authorization");
-//   if (req.method === "OPTIONS") {
-//     return res.status(200).end();
-//   }
-//   return next();   
-// });
+app.use(express.json())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header,Authorization");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  return next();   
+});
 
+const authUrl = process.env.VUE_APP_AUTH_URL;
+const restUrl = process.env.VUE_APP_REST_URL;
  
-app.get('/', function (req, res) {
-  getBearer()
-  res.json()
-})
-
-function getBearer(){
-  const restUrl = process.env.VUE_APP_REST_URL
-  const authUrl = process.env.VUE_APP_AUTH_URL
-
+app.get('/auth', function (req, res) {
   const body = {
     "grant_type": "client_credentials",
     "client_id": process.env.VUE_APP_CLIENT_ID,
@@ -31,24 +26,28 @@ function getBearer(){
     "account_id": process.env.VUE_APP_ACCOUNT_ID
   }
 
-  axios.post(authUrl + "/v2/token", body).then((res) => console.log(res))
-}
+  axios.post(authUrl + "/v2/token", body)
+    .then((reponse) => res.json({token: reponse.data}))
+    .catch((reponse) => res.json({error: "error :("}))
+})
+
+app.post('/post', function(req, res) {
+  const dataExtension = "B0B5F4DA-CAB7-4C30-89CA-110A36B5F468"
+  const token = req.body.token; //"eyJhbGciOiJIUzI1NiIsImtpZCI6IjEiLCJ2ZXIiOiIxIiwidHlwIjoiSldUIn0.eyJhY2Nlc3NfdG9rZW4iOiI3QXhPakpKbUVvMnVxVnk0cEdCSTVKVEQiLCJjbGllbnRfaWQiOiJzZ3JiNXFtZzlseGR0eTJuaHBocWZoaXoiLCJlaWQiOjcyNzUyODMsInN0YWNrX2tleSI6IlM3IiwicGxhdGZvcm1fdmVyc2lvbiI6MiwiY2xpZW50X3R5cGUiOiJTZXJ2ZXJUb1NlcnZlciJ9.KnKFkzmVX38yDbw0l0OZQRixyCbtthrXRTt4g0WHzFA.poVoj6tmIWChUARoinGdvNGEnk5QqE5hdMwXL0k619qTGTSFoZoE05JZyGxD1VqALgmrSRaCc_02BxveTvEWZngp1HA8Fxo2f8q79-DiHZ5ylgE_jo7EEaU2T5l_JYWgQmHzGgHpQxbvdMpLxnwdVOl8bLfw19jqZMPLBsiAt1lFGrR_7gOdiFP";
+  console.log(req.body)
+  const bodyParameters = {
+    "items": [{
+      "Name": req.body.form.name,
+      "Email Address": "Testing@woot.lol"
+    }]
+  };
+  const config = {
+    headers: { Authorization: `Bearer ${token}` }
+  }
+  axios.post(restUrl + `/data/v1/async/dataextensions/key:${dataExtension}/rows`, bodyParameters, config)
+    .then((res) => console.log(res.status))
+    .catch((res) => console.log(res.status))
+  res.json();
+})
  
 app.listen(3000)
-
-
-/*
-    const restUrl = process.env.VUE_APP_REST_URL
-      const dataExtension = "B0B5F4DA-CAB7-4C30-89CA-110A36B5F468"
-      const token = "eyJhbGciOiJIUzI1NiIsImtpZCI6IjEiLCJ2ZXIiOiIxIiwidHlwIjoiSldUIn0.eyJhY2Nlc3NfdG9rZW4iOiI3ak50VUd3THI4UnZkRWNvYXF0allINnEiLCJjbGllbnRfaWQiOiJzZ3JiNXFtZzlseGR0eTJuaHBocWZoaXoiLCJlaWQiOjcyNzUyODMsInN0YWNrX2tleSI6IlM3IiwicGxhdGZvcm1fdmVyc2lvbiI6MiwiY2xpZW50X3R5cGUiOiJTZXJ2ZXJUb1NlcnZlciJ9.IjCrRtsNSHPiZwIsdp0StfKaCuh-2cjrVnPZmuQlEoc.Rhx6LKEwrkh8FvdgMd96RrOmWK3qjlYZ2XVdhpwrQmDaO9ejx-oQ517kbRQowlYP00LxV_Uh4bjFYFMr0iesUdSXhvUH1n_JlXFPBiBC8BcfTbNZOepruVkf3maW5BQ2OBxAkTtgCP3DKMGE0duXg288DIV0UhjqK82c6PgYMrBdGM0EAV_bvkW";
-      const config = {
-        headers: { Authorization: `Bearer ${token}` }
-      }
-      const bodyParameters = {
-        "Name": "Bubbz from Vue",
-        "Email Address": "Testing@woot.se"
-      };
-      axios.post(restUrl + `/data/v1/async/dataextensions/key:${dataExtension}/rows`, bodyParameters, config)
-      .then(console.log).catch(console.log)
-
-*/
